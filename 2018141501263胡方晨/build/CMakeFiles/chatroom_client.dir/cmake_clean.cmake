7ab17414583a5941262918bc9bb76c2dbec44978
FangchenHu <47878934+FangchenHu@users.noreply.github.com>
2021-06-16 18:20:17 +0800

    Add files via upload

---
+++ 2018141501263胡方晨/build/CMakeFiles/chatroom_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/chatroom_client"
+  "../bin/chatroom_client.pdb"
+  "CMakeFiles/chatroom_client.dir/src/Client.cpp.o"
+  "CMakeFiles/chatroom_client.dir/src/ClientMain.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chatroom_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2018141501263胡方晨/build/CMakeFiles/chatroom_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/chatroom_server"
+  "../bin/chatroom_server.pdb"
+  "CMakeFiles/chatroom_server.dir/src/Server.cpp.o"
+  "CMakeFiles/chatroom_server.dir/src/ServerMain.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chatroom_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

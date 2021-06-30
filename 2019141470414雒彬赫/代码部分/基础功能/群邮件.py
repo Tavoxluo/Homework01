d0523d1ad3fce96ed75977a03cf1794b03abcb25
nuclearprotein <70878829+dearprotein@users.noreply.github.com>
2021-06-30 10:14:30 +0800

    Add files via upload

---
+++ 2019141470414雒彬赫/代码部分/基础功能/群邮件.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3  
+#coding: utf-8  
+import smtplib  
+from email.mime.text import MIMEText  
+  
+sender = '***'  
+receiver = ['***','****',]  
+subject = 'python email test'  
+smtpserver = 'smtp.163.com'  
+username = '***'  
+password = '***'  
+  
+msg = MIMEText('你好','plain','utf-8')  
+  
+msg['Subject'] = subject  
+  
+smtp = smtplib.SMTP()  
+smtp.connect('smtp.163.com')  
+smtp.login(username, password)  
+smtp.sendmail(sender, receiver, msg.as_string())  
+smtp.quit()
